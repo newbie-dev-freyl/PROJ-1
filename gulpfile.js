1d56042208166146be75615f841ed69376427406
newbie-dev-freyl <softwaretransjp@gmail.com>
2023-10-21 17:38:27 +0800

    added gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postCss = require('gulp-postcss');
+const cssNano = require('cssnano');
+const terser = require('gulp-terser');  
+const browserSync = require('browser-sync').create();
+
+// sass task
+function scssTask() {
+    return src('src/scss/mystyle.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postCss([cssNano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// javascrip task
+function jsTask (){
+    return src('src/js/myscript.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// browsersync task
+function browserSyncServer(done) {
+    browserSync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    done();
+}
+
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+// watch task
+function watchTask(){
+    watch('*.html', browserSyncReload);
+    watch(['src/scss/**/*.scss', 'src/js/**/*.js'], series(scssTask, jsTask, browserSyncReload));
+}
+
+
+// default gulp tasks 
+exports.default = series(
+    scssTask,
+    jsTask,
+    browserSyncServer,
+    watchTask
+);
