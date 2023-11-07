a543f21d23d634366bab6c44c355f32cbb245503
newbie-dev-freyl <softwaretransjp@gmail.com>
2023-11-08 00:55:18 +0800

    semi final solution

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,18 +1,53 @@
 const navTop = document.querySelector('.nav-top');
 const navTopToggle = navTop.previousElementSibling;
-const navTopBackButton = document.querySelectorAll('[back-button]');
+const navTopLinkAll = navTop.querySelectorAll('a');
+const navTopBackButtonAll = navTop.querySelectorAll('[back-button]');
+const navTopSubMenuAll = navTop.querySelectorAll('.nav-top__submenu');
 
 
 navTopToggle.onclick = function () {
     navTop.toggleAttribute('show');
     navTopToggle.toggleAttribute('toggled');
+
+    navTopSubMenuAll.forEach(menu => {
+        menu.removeAttribute('show');
+    })
 }
 
 
 function fn_screen_width(size) {
     if (document.documentElement.getBoundingClientRect().width > size) {
-        navTopBackButton.forEach(button => {
-            button.parentElement.style.display = "none"; 
+        navTopBackButtonAll.forEach(button => {
+            button.parentElement.style.display = "none";
+        })
+        
+        navTop.removeAttribute('show');
+        navTopToggle.removeAttribute('toggled');
+
+        navTopSubMenuAll.forEach(menu => {
+            menu.removeAttribute('show');
+        })
+
+    }  else  {
+        
+        navTopBackButtonAll.forEach(button => {
+            button.parentElement.style.display = "block";
+            const text = button.parentElement.parentElement.previousElementSibling.text;
+            button.querySelector('[title]').innerHTML = text;
+        })
+
+        navTopLinkAll.forEach(link => {
+            link.onclick = function () {
+                if (link.hasAttribute('toggle-menu')) {
+                    link.nextElementSibling.setAttribute('show','')
+                } else if (link.hasAttribute('back-button')) {
+                    link.parentElement.parentElement.removeAttribute('show')
+                } else {
+                    navTopSubMenuAll.forEach(menu => {
+                        menu.removeAttribute('show');
+                    })
+                }
+            }
         })
     }
 }
