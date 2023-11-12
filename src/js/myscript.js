05fec70e7d3fae3a52aa9e05f24020096b3ce425
newbie-dev-freyl <softwaretransjp@gmail.com>
2023-11-12 12:17:33 +0800

    refactor navtop scss from scratch

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,62 +1,84 @@
+const navTopToggleMenu = document.querySelector('.nav-top__toggle');
 const navTop = document.querySelector('.nav-top');
-const navTopToggle = navTop.previousElementSibling;
-const navTopLinkAll = navTop.querySelectorAll('a');
-const navTopBackButtonAll = navTop.querySelectorAll('[back-button]');
-const navTopSubMenuAll = navTop.querySelectorAll('.nav-top__submenu');
+const navTopSubMenuAll = document.querySelectorAll('.nav-top__submenu');
+const navTopSubMenuLinkAll = document.querySelectorAll('.nav-top__menu a');
 
+function fn_setup() {
+    navTop.setAttribute('data-transform-type', 'right');
 
-navTopToggle.onclick = function () {
+    let text;
+    navTopSubMenuLinkAll.forEach(link => {
+        if (link.hasAttribute('back-button')) {
+            text = link.parentElement.parentElement.previousElementSibling.text;
+            link.querySelector('[title]').innerHTML = text;
+        }
+    })
+}
+
+function fn_hide_navtop() {
+    navTopToggleMenu.removeAttribute('toggled');
+    navTop.removeAttribute('show');
+}
+
+function fn_show_navtop(){
+    navTopToggleMenu.toggleAttribute('toggled');
     navTop.toggleAttribute('show');
-    navTopToggle.toggleAttribute('toggled');
+}
 
+function fn_hide_navtop_submenu() {
     navTopSubMenuAll.forEach(menu => {
         menu.removeAttribute('show');
     })
 }
 
-
-function fn_screen_width(size) {
-    if (document.documentElement.getBoundingClientRect().width > size) {
-        navTopBackButtonAll.forEach(button => {
-            button.parentElement.style.display = "none";
-        })
-        
-        navTop.removeAttribute('show');
-        navTopToggle.removeAttribute('toggled');
-
-        navTopSubMenuAll.forEach(menu => {
-            menu.removeAttribute('show');
-        })
-
-    }  else  {
-        
-        navTopBackButtonAll.forEach(button => {
-            button.parentElement.style.display = "block";
-            const text = button.parentElement.parentElement.previousElementSibling.text;
-            button.querySelector('[title]').innerHTML = text;
-        })
-
-        navTopLinkAll.forEach(link => {
-            link.onclick = function () {
-                if (link.hasAttribute('toggle-menu')) {
-                    link.nextElementSibling.setAttribute('show','')
-                } else if (link.hasAttribute('back-button')) {
-                    link.parentElement.parentElement.removeAttribute('show')
-                } else {
-                    navTopSubMenuAll.forEach(menu => {
-                        menu.removeAttribute('show');
-                    })
-                }
+function fn_toggle_navtop_submenu() {
+    navTopSubMenuLinkAll.forEach(link => {
+        link.onclick = function () {
+            if (link.hasAttribute('toggle-menu')) {
+                link.nextElementSibling.setAttribute('show','')
+            } else if (link.hasAttribute('back-button')) {
+                link.parentElement.parentElement.removeAttribute('show')
+            } else {
+                fn_hide_navtop_submenu();
             }
-        })
+        }
+    })
+}
+
+function fn_screen_size(size) {
+    if (document.documentElement.getBoundingClientRect().width > size) {
+        fn_hide_navtop();
+    } else {
+        fn_toggle_navtop_submenu();
     }
 }
 
+function fn_disable_effect(secs) {
+    let resizeTimer;
+    document.body.setAttribute('effects-disabled','');
+
+    resizeTimer = setTimeout(() => {
+        document.body.removeAttribute('effects-disabled');
+    }, secs)
+}
+
+
+navTopToggleMenu.onclick = function () {
+    fn_show_navtop()
+    fn_hide_navtop_submenu();
+}
+
 window.onload = function () {
-    navTop.setAttribute('data-transform-type', 'right');
-    fn_screen_width(736);
+    fn_setup();
+    fn_screen_size(736);
 }
 
 window.onresize = function () {
-    fn_screen_width(736);
+    fn_disable_effect(200);
+    fn_screen_size(736);
 }
+
+
+
+
+
